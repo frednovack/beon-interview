606ad59710e94bd1b99917b5714eb95b6bd02fad
Fred Novack <fredynovack@gmail.com>
2022-04-11 20:06:51 -0300

    Initial Commit

---
+++ interview/interviewApp.swift
@@ -0,0 +1,17 @@
+//
+//  interviewApp.swift
+//  interview
+//
+//  Created by Fred Novack on 11/04/22.
+//
+
+import SwiftUI
+
+@main
+struct interviewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
